1d39eb8563287d4891fef3676f50fb29b7571707
Meliksah Yorulmazlar <yorulk@rpi.edu>
2024-11-27 14:06:24 -0500

    Update main.py
    new methods

--- Newspapers/Bermuda/Bermuda National Library/main.py
+++ Newspapers/Bermuda/Bermuda National Library/main.py
@@ -1,13 +1,7 @@
 # Bermuda National Library archive
-import os
-import time
-
+import requests,lxml,os,time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import requests,lxml
 from bs4 import BeautifulSoup
-
 
 class BermudaNationLibrary:
     def __init__(self):
@@ -57,7 +51,6 @@ class BermudaNationLibrary:
             number = int(h3[0].split()[-1])
             page_count = self.find_page_count(number)
 
-            count = 1
             for i in range(1,page_count+1):
                 site = f"{page}/page/{i}"
                 print(site)
@@ -96,12 +89,12 @@ class BermudaNationLibrary:
                     string = "https://bnl.contentdm.oclc.org/digital/api/collection/p16347coll6/id/237/page/0/inline/p16347coll6_237_0"
                     pdf_file = f"https://bnl.contentdm.oclc.org/digital/api/collection/{collection_type}/id/{id_type}/page/0/inline/{collection_type}_{id_type}_0"
 
-                    filename = f"{newspaper} {dates[i]} {id_type} {count}"
+                    filename = f"{newspaper} {dates[i]} {id_type}.pdf"
 
                     response = requests.get(url=pdf_file)
 
                     if response.status_code == 200:
-                        with open(f"{newspaper}/{filename}.pdf",'wb') as f:
+                        with open(f"{newspaper}/{filename}",'wb') as f:
                             f.write(response.content)
                         with open("download_results.txt","a") as f:
                             f.write(f"{filename} was downloaded.\n")
@@ -110,18 +103,99 @@ class BermudaNationLibrary:
                         with open("download_results.txt","a") as f:
                             f.write(f"{filename} was not downloaded,it had response status code {response.status_code}\n")
                         print(f"{filename} was not downloaded,it had response status code {response.status_code}")
-                    count += 1
+
     # The following method will download all the newspapers
     def download_newspapers(self):
         for newspaper in self.newspaper_dictionary:
             self.download_newspaper(newspaper)
 
+    def check_newspaper(self,newspaper:str):
+        if newspaper in self.newspaper_dictionary:
+            try:
+                items = os.listdir(newspaper)
+            except FileNotFoundError:
+                pass
+            else:
 
 
+                page = self.newspaper_dictionary[newspaper]
+                self.driver.get(page)
+                time.sleep(1)
+                soup = BeautifulSoup(self.driver.page_source, 'lxml')
+                h3 = [h3.text.replace(",", '') for h3 in soup.find_all("h3") if "Records 1-" in h3.text]
+
+                if h3:
+                    number = int(h3[0].split()[-1])
+                else:
+                    soup = BeautifulSoup(self.driver.page_source, 'lxml')
+                    h3 = [h3.text.replace(",", '') for h3 in soup.find_all("h3") if "Records 1-" in h3.text]
+                    number = int(h3[0].split()[-1])
+                page_count = self.find_page_count(number)
+
+                for i in range(1, page_count + 1):
+                    site = f"{page}/page/{i}"
+                    print(site)
+                    self.driver.get(site)
+                    time.sleep(2)
+                    soup = BeautifulSoup(self.driver.page_source, 'lxml')
+
+                    links = [f'https://bnl.contentdm.oclc.org{image['src']}' for image in soup.find_all("img", src=True) if
+                             "api" in image['src']]
+                    links = [link for link in links if requests.get(link).status_code == 200]
+                    divs = [div.text for div in soup.find_all('div', class_="MetadataField-content truncation")]
+                    print(links)
+
+                    # gets the dates of the newspapers
+                    dates = []
+                    for i in range(len(divs)):
+                        div = divs[i]
+                        if div == "PDF" or div == 'Microfilm' or div in self.newspaper_dictionary:
+                            continue
+                        elif "January" in div or "February" in div or "March" in div or "April" in div or "May" in div or "June" in div or "July" in div or "August" in div or "September" in div or "October" in div or "November" in div or "December" in div:
+                            dates.append(div)
+                        elif "-" in div:
+                            dates.append(div)
+
+                    try:
+                        os.mkdir(newspaper)
+                    except FileExistsError:
+                        pass
+
+                    for i in range(len(links)):
+                        link = links[i]
+                        response = requests.get(url=link)
+                        split_form = link.split("/")
+                        collection_type = split_form[-4]
+                        id_type = split_form[-2]
+
+                        string = "https://bnl.contentdm.oclc.org/digital/api/collection/p16347coll6/id/237/page/0/inline/p16347coll6_237_0"
+                        pdf_file = f"https://bnl.contentdm.oclc.org/digital/api/collection/{collection_type}/id/{id_type}/page/0/inline/{collection_type}_{id_type}_0"
+
+                        filename = f"{newspaper} {dates[i]} {id_type}.pdf"
+
+                        if filename not in items:
+
+                            response = requests.get(url=pdf_file)
+
+                            if response.status_code == 200:
+                                with open(f"{newspaper}/{filename}", 'wb') as f:
+                                    f.write(response.content)
+                                with open("download_results.txt", "a") as f:
+                                    f.write(f"{filename} was downloaded.\n")
+                                print(f"{filename} was downloaded.")
+                            else:
+                                with open("download_results.txt", "a") as f:
+                                    f.write(
+                                        f"{filename} was not downloaded,it had response status code {response.status_code}\n")
+                                print(f"{filename} was not downloaded,it had response status code {response.status_code}")
+
+    def check_newspapers(self):
+        for newspaper in self.newspaper_dictionary:
+            self.check_newspaper(newspaper)
 
 
 
 
 if __name__ == "__main__":
     bnl = BermudaNationLibrary()
-    bnl.download_newspapers()
+    bnl.check_newspaper(newspaper='Bermuda Beacon')
