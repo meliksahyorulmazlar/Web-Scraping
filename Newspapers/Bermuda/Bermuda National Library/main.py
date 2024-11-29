bb843a47f63e1dd0cfcef1e951e945b065d2f6a8
Meliksah Yorulmazlar <yorulk@rpi.edu>
2024-11-29 00:36:07 -0500

    Update main.py

--- Newspapers/Bermuda/Bermuda National Library/main.py
+++ Newspapers/Bermuda/Bermuda National Library/main.py
@@ -42,7 +42,9 @@ class BermudaNationLibrary:
 
     # The following method will download a newspaper that is on the website
     def download_newspaper(self,newspaper:str):
-        if newspaper in self.newspaper_dictionary:
+        if newspaper == 'Royal Gazette (1784 - 1964)':
+            self.download_royal_gazette()
+        elif newspaper in self.newspaper_dictionary:
             page = self.newspaper_dictionary[newspaper]
             self.driver.get(page)
             time.sleep(1)
@@ -73,6 +75,11 @@ class BermudaNationLibrary:
                         dates.append(div)
                     elif "-" in div:
                         dates.append(div)
+                    elif len(div) == 4:
+                        numbers = ['0','1','2','3','4','5','6','7','8','9']
+                        total = sum([1 for char in div if char in numbers])
+                        if total == 4:
+                            dates.append(div)
 
                 try:
                     os.mkdir(newspaper)
@@ -109,16 +116,15 @@ class BermudaNationLibrary:
         for newspaper in self.newspaper_dictionary:
             self.download_newspaper(newspaper)
 
-    # The following method will check if all the newspapers have been downloaded
     def check_newspaper(self,newspaper:str):
-        if newspaper in self.newspaper_dictionary:
+        if newspaper == 'Royal Gazette (1784 - 1964)':
+            self.check_royal_gazette()
+        elif newspaper in self.newspaper_dictionary:
             try:
                 items = os.listdir(newspaper)
             except FileNotFoundError:
-                self.download_newspaper(newspaper)
+                pass
             else:
-
-
                 page = self.newspaper_dictionary[newspaper]
                 self.driver.get(page)
                 time.sleep(1)
@@ -156,6 +162,11 @@ class BermudaNationLibrary:
                             dates.append(div)
                         elif "-" in div:
                             dates.append(div)
+                        elif len(div) == 4:
+                            numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+                            total = sum([1 for char in div if char in numbers])
+                            if total == 4:
+                                dates.append(div)
 
                     try:
                         os.mkdir(newspaper)
@@ -169,7 +180,7 @@ class BermudaNationLibrary:
                         collection_type = split_form[-4]
                         id_type = split_form[-2]
 
-                        string = "https://bnl.contentdm.oclc.org/digital/api/collection/p16347coll6/id/237/page/0/inline/p16347coll6_237_0"
+                        #string = "https://bnl.contentdm.oclc.org/digital/api/collection/p16347coll6/id/237/page/0/inline/p16347coll6_237_0"
                         pdf_file = f"https://bnl.contentdm.oclc.org/digital/api/collection/{collection_type}/id/{id_type}/page/0/inline/{collection_type}_{id_type}_0"
 
                         filename = f"{newspaper} {dates[i]} {id_type}.pdf"
@@ -189,15 +200,166 @@ class BermudaNationLibrary:
                                     f.write(
                                         f"{filename} was not downloaded,it had response status code {response.status_code}\n")
                                 print(f"{filename} was not downloaded,it had response status code {response.status_code}")
-                                
-    # The following method will check if there were any new additional newspapers
+
+    # The following method will check all the newspapers
     def check_newspapers(self):
         for newspaper in self.newspaper_dictionary:
             self.check_newspaper(newspaper)
 
+    # A specific method had to be coded to download one of the methods
+    def download_royal_gazette(self):
+        newspaper = 'Royal Gazette (1784 - 1964)'
+        page = 'https://bnl.contentdm.oclc.org/digital/collection/BermudaNP02/search'
+        self.driver.get(page)
+        time.sleep(1)
+        soup = BeautifulSoup(self.driver.page_source, 'lxml')
+        h3 = [h3.text.replace(",", '') for h3 in soup.find_all("h3") if "Records 1-" in h3.text]
+        number = int(h3[0].split()[-1])
+        page_count = self.find_page_count(number)
+        for i in range(1, page_count + 1):
+            site = f"{page}/page/{i}"
+            self.driver.get(site)
+            time.sleep(2)
+            soup = BeautifulSoup(self.driver.page_source, 'lxml')
+
+            links = [f'https://bnl.contentdm.oclc.org{link['href']}' for link in soup.find_all("a", href=True) if "digital/collection" in link['href'] and '/rec/' in link['href']]
+            print(links)
+            links = [link for link in links if requests.get(link).status_code == 200]
+            divs = [div.text for div in soup.find_all('div', class_="MetadataField-content truncation")]
+
+            # gets the dates of the newspapers
+            dates = []
+            for i in range(len(divs)):
+                div = divs[i]
+                if div == "PDF" or div == 'Microfilm' or div in self.newspaper_dictionary:
+                    continue
+                elif "January" in div or "February" in div or "March" in div or "April" in div or "May" in div or "June" in div or "July" in div or "August" in div or "September" in div or "October" in div or "November" in div or "December" in div:
+                    dates.append(div)
+                elif "-" in div:
+                    dates.append(div)
+                elif len(div) == 4:
+                    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+                    total = sum([1 for char in div if char in numbers])
+                    if total == 4:
+                        dates.append(div)
+
+            try:
+                os.mkdir(newspaper)
+            except FileExistsError:
+                pass
+
+            for i in range(len(links)):
+                date = dates[i]
+                try:
+                    os.mkdir(f"{newspaper}/{date}")
+                except FileExistsError:
+                    pass
+                link = links[i]
+                self.driver.get(link)
+                time.sleep(1)
+                soup = BeautifulSoup(self.driver.page_source,'lxml')
+                images = [int(image['src'].split("/")[-2]) for image in soup.find_all('img',src=True) if 'api' in image['src']]
+                numbers =[]
+                for n in images:
+                    if n not in numbers:
+                        numbers.append(n)
+
+                for n in numbers:
+                    new_link = f"https://bnl.contentdm.oclc.org/digital/download/collection/BermudaNP02/id/{n}/size/full"
+                    response = requests.get(new_link)
+                    if response.status_code == 200:
+                        with open(f'{newspaper}/{date}/{n}.jpg','wb') as f:
+                            f.write(response.content)
+                        with open('download_results.txt','a') as f:
+                            f.write(f'{newspaper}/{date}/{n}.jpg was downloaded.\n')
+                        print(f'{newspaper}/{date}/{n}.jpg was downloaded.')
+                    else:
+                        with open('download_results.txt','a') as f:
+                            f.write(f'{newspaper}/{date}/{n}.jpg was not downloaded,it had response status code {response.status_code}\n')
+                        print(f'{newspaper}/{date}/{n}.jpg was downloaded,it had response status code {response.status_code}')
 
 
+    # A specific method had to be coded to check one of the newspapers
+    def check_royal_gazette(self):
+        page = 'https://bnl.contentdm.oclc.org/digital/collection/BermudaNP02/search'
+        try:
+            days = os.listdir('Royal Gazette (1784 - 1964)')
+        except FileNotFoundError:
+            self.download_royal_gazette()
+        else:
+            newspaper = 'Royal Gazette (1784 - 1964)'
+            page = 'https://bnl.contentdm.oclc.org/digital/collection/BermudaNP02/search'
+            self.driver.get(page)
+            time.sleep(1)
+            soup = BeautifulSoup(self.driver.page_source, 'lxml')
+            h3 = [h3.text.replace(",", '') for h3 in soup.find_all("h3") if "Records 1-" in h3.text]
+            number = int(h3[0].split()[-1])
+            page_count = self.find_page_count(number)
+            for i in range(1, page_count + 1):
+                site = f"{page}/page/{i}"
+                self.driver.get(site)
+                time.sleep(2)
+                soup = BeautifulSoup(self.driver.page_source, 'lxml')
+
+                links = [f'https://bnl.contentdm.oclc.org{link['href']}' for link in soup.find_all("a", href=True) if
+                         "digital/collection" in link['href'] and '/rec/' in link['href']]
+                print(links)
+                links = [link for link in links if requests.get(link).status_code == 200]
+                divs = [div.text for div in soup.find_all('div', class_="MetadataField-content truncation")]
+
+                # gets the dates of the newspapers
+                dates = []
+                for i in range(len(divs)):
+                    div = divs[i]
+                    if div == "PDF" or div == 'Microfilm' or div in self.newspaper_dictionary:
+                        continue
+                    elif "January" in div or "February" in div or "March" in div or "April" in div or "May" in div or "June" in div or "July" in div or "August" in div or "September" in div or "October" in div or "November" in div or "December" in div:
+                        dates.append(div)
+                    elif "-" in div:
+                        dates.append(div)
+                    elif len(div) == 4:
+                        numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+                        total = sum([1 for char in div if char in numbers])
+                        if total == 4:
+                            dates.append(div)
+
+                try:
+                    os.mkdir(newspaper)
+                except FileExistsError:
+                    pass
+
+                for i in range(len(links)):
+                    date = dates[i]
+
+                    try:
+                        os.mkdir(f"{newspaper}/{date}")
+                    except FileExistsError:
+                        pass
+                    link = links[i]
+                    self.driver.get(link)
+                    time.sleep(1)
+                    soup = BeautifulSoup(self.driver.page_source, 'lxml')
+                    images = [int(image['src'].split("/")[-2]) for image in soup.find_all('img', src=True) if'api' in image['src']]
+                    numbers = []
+                    for n in images:
+                        if n not in numbers:
+                            numbers.append(n)
+
+                    for n in numbers:
+                        if f'{n}.jpg' not in os.listdir(f"{newspaper}/{date}"):
+                            new_link = f"https://bnl.contentdm.oclc.org/digital/download/collection/BermudaNP02/id/{n}/size/full"
+                            response = requests.get(new_link)
+                            if response.status_code == 200:
+                                with open(f'{newspaper}/{date}/{n}.jpg', 'wb') as f:
+                                    f.write(response.content)
+                                with open('download_results.txt', 'a') as f:
+                                    f.write(f'{newspaper}/{date}/{n}.jpg was downloaded.\n')
+                                print(f'{newspaper}/{date}/{n}.jpg was downloaded.')
+                            else:
+                                with open('download_results.txt', 'a') as f:
+                                    f.write(f'{newspaper}/{date}/{n}.jpg was not downloaded,it had response status code {response.status_code}\n')
+                                print(f'{newspaper}/{date}/{n}.jpg was downloaded,it had response status code {response.status_code}')
 
 if __name__ == "__main__":
     bnl = BermudaNationLibrary()
-    bnl.check_newspaper(newspaper='Bermuda Beacon')
+    bnl.check_royal_gazette()
