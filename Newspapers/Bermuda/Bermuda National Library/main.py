d4b4a6fca6de791bd78d8e24606e9e7bc5b14b14
Meliksah Yorulmazlar <yorulk@rpi.edu>
2024-12-02 21:57:25 -0500

    Update main.py
    new code as most pdfs do not get downloaded

--- Newspapers/Bermuda/Bermuda National Library/main.py
+++ Newspapers/Bermuda/Bermuda National Library/main.py
@@ -1,5 +1,5 @@
 # Bermuda National Library archive
-import requests,lxml,os,time
+import requests,lxml,os,time,datetime
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
@@ -8,6 +8,7 @@ class BermudaNationLibrary:
         self.start_driver()
         self.newspaper_dictionary = {}
         self.get_newspapers()
+        self.first_date = datetime.datetime(day=2,month=1,year=1900)
 
     # The following method will start the selenium webdriver
     def start_driver(self):
@@ -216,7 +217,7 @@ class BermudaNationLibrary:
         h3 = [h3.text.replace(",", '') for h3 in soup.find_all("h3") if "Records 1-" in h3.text]
         number = int(h3[0].split()[-1])
         page_count = self.find_page_count(number)
-        for i in range(1, page_count + 1):
+        for i in range(295, page_count + 1):
             site = f"{page}/page/{i}"
             self.driver.get(site)
             time.sleep(2)
@@ -250,33 +251,65 @@ class BermudaNationLibrary:
 
             for i in range(len(links)):
                 date = dates[i]
-                try:
-                    os.mkdir(f"{newspaper}/{date}")
-                except FileExistsError:
-                    pass
-                link = links[i]
-                self.driver.get(link)
-                time.sleep(1)
-                soup = BeautifulSoup(self.driver.page_source,'lxml')
-                images = [int(image['src'].split("/")[-2]) for image in soup.find_all('img',src=True) if 'api' in image['src']]
-                numbers =[]
-                for n in images:
-                    if n not in numbers:
-                        numbers.append(n)
+                datetime_format = date.split('-')
+                datetime_format = datetime.datetime(day=int(datetime_format[-1]),month=int(datetime_format[1]),year=int(datetime_format[0]))
 
-                for n in numbers:
-                    new_link = f"https://bnl.contentdm.oclc.org/digital/download/collection/BermudaNP02/id/{n}/size/full"
-                    response = requests.get(new_link)
+                if datetime_format >= self.first_date:
+                    print('hi')
+                    'https://bnl.contentdm.oclc.org/digital/collection/BermudaNP02/id/30273/rec/5899#'
+                    'https://bnl.contentdm.oclc.org/digital/api/collection/BermudaNP02/id/30273/page/0/inline/BermudaNP02_30273_0'
+                    link = links[i]
+                    response = requests.get(url=link)
+                    split_form = link.split("/")
+                    collection_type = split_form[-4]
+                    id_type = split_form[-2]
+
+                    pdf_file = f"https://bnl.contentdm.oclc.org/digital/api/collection/{collection_type}/id/{id_type}/page/0/inline/{collection_type}_{id_type}_0"
+
+                    number = link.split('/')[-3]
+                    filename = f"{newspaper} {dates[i]} {number}.pdf"
+                    pdf_file = f'https://bnl.contentdm.oclc.org/digital/api/collection/BermudaNP02/id/{number}/page/0/inline/BermudaNP02_{number}_0'
+                    response = requests.get(url=pdf_file)
                     if response.status_code == 200:
-                        with open(f'{newspaper}/{date}/{n}.jpg','wb') as f:
+                        with open(f"{newspaper}/{filename}", 'wb') as f:
                             f.write(response.content)
-                        with open('download_results.txt','a') as f:
-                            f.write(f'{newspaper}/{date}/{n}.jpg was downloaded.\n')
-                        print(f'{newspaper}/{date}/{n}.jpg was downloaded.')
+                        with open("download_results.txt", "a") as f:
+                            f.write(f"{filename} was downloaded.\n")
+                        print(f"{filename} was downloaded.")
                     else:
-                        with open('download_results.txt','a') as f:
-                            f.write(f'{newspaper}/{date}/{n}.jpg was not downloaded,it had response status code {response.status_code}\n')
-                        print(f'{newspaper}/{date}/{n}.jpg was downloaded,it had response status code {response.status_code}')
+                        with open("download_results.txt", "a") as f:
+                            f.write(
+                                f"{filename} was not downloaded,it had response status code {response.status_code}\n")
+                        print(f"{filename} was not downloaded,it had response status code {response.status_code}")
+
+                else:
+                    try:
+                        os.mkdir(f"{newspaper}/{date}")
+                    except FileExistsError:
+                        pass
+                    link = links[i]
+                    self.driver.get(link)
+                    time.sleep(1)
+                    soup = BeautifulSoup(self.driver.page_source,'lxml')
+                    images = [int(image['src'].split("/")[-2]) for image in soup.find_all('img',src=True) if 'api' in image['src']]
+                    numbers =[]
+                    for n in images:
+                        if n not in numbers:
+                            numbers.append(n)
+
+                    for n in numbers:
+                        new_link = f"https://bnl.contentdm.oclc.org/digital/download/collection/BermudaNP02/id/{n}/size/full"
+                        response = requests.get(new_link)
+                        if response.status_code == 200:
+                            with open(f'{newspaper}/{date}/{n}.jpg','wb') as f:
+                                f.write(response.content)
+                            with open('download_results.txt','a') as f:
+                                f.write(f'{newspaper}/{date}/{n}.jpg was downloaded.\n')
+                            print(f'{newspaper}/{date}/{n}.jpg was downloaded.')
+                        else:
+                            with open('download_results.txt','a') as f:
+                                f.write(f'{newspaper}/{date}/{n}.jpg was not downloaded,it had response status code {response.status_code}\n')
+                            print(f'{newspaper}/{date}/{n}.jpg was downloaded,it had response status code {response.status_code}')
 
 
     # A specific method had to be coded to check one of the newspapers
@@ -295,14 +328,13 @@ class BermudaNationLibrary:
             h3 = [h3.text.replace(",", '') for h3 in soup.find_all("h3") if "Records 1-" in h3.text]
             number = int(h3[0].split()[-1])
             page_count = self.find_page_count(number)
-            for i in range(1, page_count + 1):
+            for i in range(295, page_count + 1):
                 site = f"{page}/page/{i}"
                 self.driver.get(site)
                 time.sleep(2)
                 soup = BeautifulSoup(self.driver.page_source, 'lxml')
 
-                links = [f'https://bnl.contentdm.oclc.org{link['href']}' for link in soup.find_all("a", href=True) if
-                         "digital/collection" in link['href'] and '/rec/' in link['href']]
+                links = [f'https://bnl.contentdm.oclc.org{link['href']}' for link in soup.find_all("a", href=True) if "digital/collection" in link['href'] and '/rec/' in link['href']]
                 print(links)
                 links = [link for link in links if requests.get(link).status_code == 200]
                 divs = [div.text for div in soup.find_all('div', class_="MetadataField-content truncation")]
@@ -330,35 +362,66 @@ class BermudaNationLibrary:
 
                 for i in range(len(links)):
                     date = dates[i]
+                    datetime_format = date.split('-')
+                    datetime_format = datetime.datetime(day=int(datetime_format[-1]), month=int(datetime_format[1]),year=int(datetime_format[0]))
+                    print(datetime_format,self.first_date,datetime_format >= self.first_date)
+                    if datetime_format >= self.first_date:
+                        print('hi')
+                        'https://bnl.contentdm.oclc.org/digital/collection/BermudaNP02/id/30273/rec/5899#'
+                        'https://bnl.contentdm.oclc.org/digital/api/collection/BermudaNP02/id/30273/page/0/inline/BermudaNP02_30273_0'
+                        link = links[i]
+                        response = requests.get(url=link)
+                        split_form = link.split("/")
+                        collection_type = split_form[-4]
+                        id_type = split_form[-2]
 
-                    try:
-                        os.mkdir(f"{newspaper}/{date}")
-                    except FileExistsError:
-                        pass
-                    link = links[i]
-                    self.driver.get(link)
-                    time.sleep(1)
-                    soup = BeautifulSoup(self.driver.page_source, 'lxml')
-                    images = [int(image['src'].split("/")[-2]) for image in soup.find_all('img', src=True) if'api' in image['src']]
-                    numbers = []
-                    for n in images:
-                        if n not in numbers:
-                            numbers.append(n)
+                        pdf_file = f"https://bnl.contentdm.oclc.org/digital/api/collection/{collection_type}/id/{id_type}/page/0/inline/{collection_type}_{id_type}_0"
 
-                    for n in numbers:
-                        if f'{n}.jpg' not in os.listdir(f"{newspaper}/{date}"):
-                            new_link = f"https://bnl.contentdm.oclc.org/digital/download/collection/BermudaNP02/id/{n}/size/full"
-                            response = requests.get(new_link)
+                        number = link.split('/')[-3]
+                        filename = f"{newspaper} {dates[i]} {number}.pdf"
+                        pdf_file = f'https://bnl.contentdm.oclc.org/digital/api/collection/BermudaNP02/id/{number}/page/0/inline/BermudaNP02_{number}_0'
+                        if filename not in os.listdir('Royal Gazette (1784 - 1964)'):
+                            response = requests.get(url=pdf_file)
                             if response.status_code == 200:
-                                with open(f'{newspaper}/{date}/{n}.jpg', 'wb') as f:
+                                with open(f"{newspaper}/{filename}", 'wb') as f:
                                     f.write(response.content)
-                                with open('download_results.txt', 'a') as f:
-                                    f.write(f'{newspaper}/{date}/{n}.jpg was downloaded.\n')
-                                print(f'{newspaper}/{date}/{n}.jpg was downloaded.')
+                                with open("download_results.txt", "a") as f:
+                                    f.write(f"{filename} was downloaded.\n")
+                                print(f"{filename} was downloaded.")
                             else:
-                                with open('download_results.txt', 'a') as f:
-                                    f.write(f'{newspaper}/{date}/{n}.jpg was not downloaded,it had response status code {response.status_code}\n')
-                                print(f'{newspaper}/{date}/{n}.jpg was downloaded,it had response status code {response.status_code}')
+                                with open("download_results.txt", "a") as f:
+                                    f.write(
+                                        f"{filename} was not downloaded,it had response status code {response.status_code}\n")
+                                print(f"{filename} was not downloaded,it had response status code {response.status_code}")
+                    else:
+                        try:
+                            os.mkdir(f"{newspaper}/{date}")
+                        except FileExistsError:
+                            pass
+                        link = links[i]
+                        self.driver.get(link)
+                        time.sleep(1)
+                        soup = BeautifulSoup(self.driver.page_source, 'lxml')
+                        images = [int(image['src'].split("/")[-2]) for image in soup.find_all('img', src=True) if'api' in image['src']]
+                        numbers = []
+                        for n in images:
+                            if n not in numbers:
+                                numbers.append(n)
+
+                        for n in numbers:
+                            if f'{n}.jpg' not in os.listdir(f"{newspaper}/{date}"):
+                                new_link = f"https://bnl.contentdm.oclc.org/digital/download/collection/BermudaNP02/id/{n}/size/full"
+                                response = requests.get(new_link)
+                                if response.status_code == 200:
+                                    with open(f'{newspaper}/{date}/{n}.jpg', 'wb') as f:
+                                        f.write(response.content)
+                                    with open('download_results.txt', 'a') as f:
+                                        f.write(f'{newspaper}/{date}/{n}.jpg was downloaded.\n')
+                                    print(f'{newspaper}/{date}/{n}.jpg was downloaded.')
+                                else:
+                                    with open('download_results.txt', 'a') as f:
+                                        f.write(f'{newspaper}/{date}/{n}.jpg was not downloaded,it had response status code {response.status_code}\n')
+                                    print(f'{newspaper}/{date}/{n}.jpg was downloaded,it had response status code {response.status_code}')
 
 if __name__ == "__main__":
     bnl = BermudaNationLibrary()
