2c6d0ac646c22806c7735fa6c789504bf42d35a5
Meliksah Yorulmazlar <yorulk@rpi.edu>
2025-01-02 02:13:45 -0500

    Update main.py
    final version there should be no problem and can finally download the ones from 1920-1944

--- Newspapers/Hungary/Délmagyarország/main.py
+++ Newspapers/Hungary/Délmagyarország/main.py
@@ -10,6 +10,7 @@ class Delmagyarorszag:
         self.year_dictionary = {}
         self.gather_data()
 
+
     # The following method will gather the data on the archive
     def gather_data(self):
         soup = BeautifulSoup(requests.get(url=self.main_page).text, 'lxml')
@@ -25,38 +26,55 @@ class Delmagyarorszag:
         if year in self.year_dictionary:
             os.mkdir(f"{year}")
             site = self.year_dictionary[year]
-            soup = BeautifulSoup(requests.get(url=site).text, 'lxml')
-            links = []
-            for n in soup.find_all('a', href=True):
-                if 'http://dmarchiv.bibl.u-szeged.hu/' in n['href']:
-                        links.append(n['href'])
-            print(links)
-            links = links[1:]
-            for i in range(len(links)):
-                link = links[i]
-                new_soup = BeautifulSoup(requests.get(url=link).text, 'lxml')
-                pdf_links = [link['href'] for link in new_soup.find_all('a', href=True) if '.pdf' in link['href'] and "(" in link.text]
-                pdf_link = pdf_links[0]
-                filename = pdf_link.split("/")[-1]
-                number = i + 1
-                if number >= 100:
-                    count_string = f"{number}"
-                elif number >= 10:
-                    count_string = f"0{number}"
-                else:
-                    count_string = f"00{number}"
-                response = requests.get(url=pdf_link)
-                if response.status_code == 200:
-                    with open(f"{year}/{filename}", 'wb') as f:
-                        f.write(response.content)
-                    with open('download_results.txt', 'a') as f:
-                        f.write(f"{year}/{filename} was downloaded.\n")
-                    print(f"{year}/{filename} was downloaded.")
-                else:
-                    with open('download_results.txt', 'a') as f:
-                        f.write(
-                            f"{year}/{filename} was not downloaded, it had response status code {response.status_code}.\n")
-                    print(f"{year}/{filename} was not downloaded, it had response status code {response.status_code}.")
+
+            try:
+                soup = BeautifulSoup(requests.get(url=site).text, 'lxml')
+            except requests.exceptions.ConnectionError:
+                self.check_year(year)
+            except requests.exceptions.ReadTimeout:
+                self.check_year(year)
+            else:
+                links = [n['href'] for n in soup.find_all('a', href=True)]
+                links = links[14:-5]
+                codes = [link.split("/")[-2] for link in links]
+                for i in range(len(links)):
+                    link = links[i]
+                    try:
+                        new_soup = BeautifulSoup(requests.get(url=link).text,'lxml')
+                    except requests.exceptions.ConnectionError:
+                        self.check_year(year)
+                    except requests.exceptions.ReadTimeout:
+                        self.check_year(year)
+                    else:
+                        pdf_links = [link['href'] for link in new_soup.find_all('a',href=True) if '.pdf' in link['href'] and "(" in link.text ]
+                        pdf_link = pdf_links[0]
+                        pdf_name = pdf_links[0].split("/")[-1].replace(".pdf", '')
+                        code = codes[i]
+                        filename = f"{pdf_name}-{code}.pdf"
+                        number = i + 1
+                        if number >= 100:
+                            count_string = f"{number}"
+                        elif number >= 10:
+                            count_string = f"0{number}"
+                        else:
+                            count_string = f"00{number}"
+                        try:
+                            response = requests.get(url=pdf_link)
+                        except requests.exceptions.ConnectionError:
+                            self.check_year(year)
+                        except requests.exceptions.ReadTimeout:
+                            self.check_year(year)
+                        else:
+                            if response.status_code == 200:
+                                with open(f"{year}/{filename}", 'wb') as f:
+                                    f.write(response.content)
+                                with open('download_results.txt', 'a') as f:
+                                    f.write(f"{year}/{filename} was downloaded.\n")
+                                print(f"{year}/{filename} was downloaded.")
+                            else:
+                                with open('download_results.txt', 'a') as f:
+                                    f.write(f"{year}/{filename} was not downloaded, it had response status code {response.status_code}.\n")
+                                print(f"{year}/{filename} was not downloaded, it had response status code {response.status_code}.")
 
     # The following method will download all the years
     def download_years(self):
@@ -71,38 +89,57 @@ class Delmagyarorszag:
             except FileExistsError:
                 pass
             site = self.year_dictionary[year]
-            soup = BeautifulSoup(requests.get(url=site).text, 'lxml')
-            links = []
-            for n in soup.find_all('a', href=True):
-                if 'http://dmarchiv.bibl.u-szeged.hu/' in n['href']:
-                        links.append(n['href'])
-            print(links)
-            links = links[1:]
-            for i in range(len(links)):
-                link = links[i]
-                new_soup = BeautifulSoup(requests.get(url=link).text,'lxml')
-                pdf_links = [link['href'] for link in new_soup.find_all('a',href=True) if '.pdf' in link['href'] and "(" in link.text ]
-                pdf_link = pdf_links[0]
-                filename = pdf_link.split("/")[-1]
-                number = i + 1
-                if number >= 100:
-                    count_string = f"{number}"
-                elif number >= 10:
-                    count_string = f"0{number}"
-                else:
-                    count_string = f"00{number}"            
-                if filename not in os.listdir(f"{year}"):
-                    response = requests.get(url=pdf_link)
-                    if response.status_code == 200:
-                        with open(f"{year}/{filename}", 'wb') as f:
-                            f.write(response.content)
-                        with open('download_results.txt', 'a') as f:
-                            f.write(f"{year}/{filename} was downloaded.\n")
-                        print(f"{year}/{filename} was downloaded.")
+            try:
+                soup = BeautifulSoup(requests.get(url=site).text, 'lxml')
+            except requests.exceptions.ConnectionError:
+                self.check_year(year)
+            except requests.exceptions.ReadTimeout:
+                self.check_year(year)
+            else:
+                links = [n['href'] for n in soup.find_all('a', href=True)]
+                links = links[14:-5]
+                existing = [item.split("-")[-1].replace(".pdf",'') for item in os.listdir(f"{year}")]
+                links = [link for link in links if link.split("/")[-2] not in existing]
+                print(len(links))
+                for i in range(len(links)):
+                    link = links[i]
+                    try:
+                        new_soup = BeautifulSoup(requests.get(url=link).text,'lxml')
+                    except requests.exceptions.ConnectionError:
+                        self.check_year(year)
+                    except requests.exceptions.ReadTimeout:
+                        self.check_year(year)
                     else:
-                        with open('download_results.txt', 'a') as f:
-                            f.write(f"{year}/{filename} was not downloaded, it had response status code {response.status_code}.\n")
-                        print(f"{year}/{filename} was not downloaded, it had response status code {response.status_code}.")
+                        pdf_links = [link['href'] for link in new_soup.find_all('a',href=True) if '.pdf' in link['href'] and "(" in link.text ]
+                        pdf_link = pdf_links[0]
+                        pdf_name = pdf_links[0].split("/")[-1].replace(".pdf", '')
+                        code = link.split("/")[-2]
+                        filename = f"{pdf_name}-{code}.pdf"
+                        number = i + 1
+                        if number >= 100:
+                            count_string = f"{number}"
+                        elif number >= 10:
+                            count_string = f"0{number}"
+                        else:
+                            count_string = f"00{number}"
+                        if filename not in os.listdir(f"{year}"):
+                            try:
+                                response = requests.get(url=pdf_link)
+                            except requests.exceptions.ConnectionError:
+                                self.check_year(year)
+                            except requests.exceptions.ReadTimeout:
+                                self.check_year(year)
+                            else:
+                                if response.status_code == 200:
+                                    with open(f"{year}/{filename}", 'wb') as f:
+                                        f.write(response.content)
+                                    with open('download_results.txt', 'a') as f:
+                                        f.write(f"{year}/{filename} was downloaded.\n")
+                                    print(f"{year}/{filename} was downloaded.")
+                                else:
+                                    with open('download_results.txt', 'a') as f:
+                                        f.write(f"{year}/{filename} was not downloaded, it had response status code {response.status_code}.\n")
+                                    print(f"{year}/{filename} was not downloaded, it had response status code {response.status_code}.")
 
     # The following method will print all the years on the archive
     def print_years(self):
@@ -117,4 +154,4 @@ class Delmagyarorszag:
 
 if __name__ == "__main__":
     delmagyarorszag = Delmagyarorszag()
-    delmagyarorszag.check_year(2003)
+    delmagyarorszag.check_years()
