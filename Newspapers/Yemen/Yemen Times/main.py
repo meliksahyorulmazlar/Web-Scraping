9d11021ca70505ed8e81c2232866b1b3e2b667b0
Meliksah Yorulmazlar <yorulk@rpi.edu>
2025-01-11 04:57:32 -0500

    Update main.py
    final version with a check method

--- Newspapers/Yemen/Yemen Times/main.py
+++ Newspapers/Yemen/Yemen Times/main.py
@@ -8,14 +8,18 @@ from bs4 import BeautifulSoup
 
 
 class YemenTimes:
-    def download(self):
+    def __init__(self):
         with open("yemen_times_final.txt","r") as f:
             download_links = f.readlines()
+        self.download_links = [link.strip('\n') for link in download_links]
 
-        download_links = [link.strip('\n') for link in download_links]
-
-        os.makedirs("Yemen Times Newspapers")
-        for download_link in download_links:
+    # The following method will download all the newspapers
+    def download_all(self):
+        try:
+            os.mkdir("Yemen Times Newspapers")
+        except FileExistsError:
+            pass
+        for download_link in self.download_links:
             response = requests.get(url=download_link)
             filename = download_link.split("/")[-1]
 
@@ -30,6 +34,28 @@ class YemenTimes:
                     f.write(f"{filename} was not downloaded, it had response status code {response.status_code}")
                 print(f"{filename} was not downloaded, it had response status code {response.status_code}")
 
+    #The following method will check if all the pdfs have been downloaded or not
+    def check_all(self):
+        try:
+            os.mkdir("Yemen Times Newspapers")
+        except FileExistsError:
+            pass
+        for download_link in self.download_links:
+            filename = download_link.split("/")[-1]
+            print(filename)
+            if filename not in os.listdir('Yemen Times Newspapers'):
+                response = requests.get(url=download_link)
+                if response.status_code == 200:
+                    with open(f"Yemen Times Newspapers/{filename}","wb") as f:
+                        f.write(response.content)
+                    with open("download_results.txt","a") as f:
+                        f.write(f"{filename} was downloaded\n")
+                    print(f"{filename} was downloaded")
+                else:
+                    with open("download_results.txt","a") as f:
+                        f.write(f"{filename} was not downloaded, it had response status code {response.status_code}.\n")
+                    print(f"{filename} was not downloaded, it had response status code {response.status_code}")
+
 if __name__  == "__main__":
     yt = YemenTimes()
-    yt.download()
+    yt.check_all()
