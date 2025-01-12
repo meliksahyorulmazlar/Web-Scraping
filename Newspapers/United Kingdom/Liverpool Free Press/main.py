775824ca624c2bed19745fb23b7cb86df641a02e
Meliksah Yorulmazlar <yorulk@rpi.edu>
2025-01-12 00:47:56 -0500

    Update main.py

--- Newspapers/United Kingdom/Liverpool Free Press/main.py
+++ Newspapers/United Kingdom/Liverpool Free Press/main.py
@@ -7,6 +7,36 @@ class LiverpoolFreePress:
     def __init__(self):
         self.site = 'https://freepressarchive.com/issues/archives.html'
 
+    def download_newspapers(self):
+        soup = BeautifulSoup(requests.get(url=self.site).text, 'lxml')
+        sites = [f"https://freepressarchive.com/issues/{site['href']}" for site in soup.find_all('a', href=True) if
+                 'issue' in site['href']]
+        for site in sites:
+            new_soup = BeautifulSoup(requests.get(url=site).text, 'lxml')
+            pdfs = [pdf['href'] for pdf in new_soup.find_all('a', href=True) if '.pdf' in pdf['href']]
+            paper_number = site.split("issue")[-1].replace(".html", '')
+            try:
+                os.mkdir(paper_number)
+            except FileExistsError:
+                pass
+            for pdf in pdfs:
+                filename = pdf.split("/")[-1]
+                pdf = f'https://freepressarchive.com/issues/{pdf}'
+                response = requests.get(url=pdf)
+                if response.status_code == 200:
+                    with open(f'{paper_number}/{filename}', 'wb') as f:
+                        f.write(response.content)
+                    with open('download_results.txt', 'a') as f:
+                        f.write(f"{paper_number}/{filename} was downloaded.\n")
+                    print(f"{paper_number}/{filename} was downloaded.")
+                else:
+                    with open('download_results.txt', 'a') as f:
+                        f.write(
+                            f"{paper_number}/{filename} was not downloaded,it had response status code {response.status_code}\n")
+                    print(
+                        f"{paper_number}/{filename} was not downloaded,it had response status code {response.status_code}")
+
+    # The following method will download the missing newspapers
     def check_papers(self):
         soup = BeautifulSoup(requests.get(url=self.site).text,'lxml')
         sites = [f"https://freepressarchive.com/issues/{site['href']}" for site in soup.find_all('a',href=True) if 'issue' in site['href']]
@@ -20,8 +50,8 @@ class LiverpoolFreePress:
                 pass
             for pdf in pdfs:
                 filename = pdf.split("/")[-1]
-                pdf = f'https://freepressarchive.com/issues/{pdf}'
                 if filename not in os.listdir(paper_number):
+                    pdf = f'https://freepressarchive.com/issues/{pdf}'
                     response = requests.get(url=pdf)
                     if response.status_code == 200:
                         with open(f'{paper_number}/{filename}','wb') as f:
