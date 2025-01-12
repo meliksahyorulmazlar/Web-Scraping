e762a56de4f579f90b835e46da005c062a2f473a
Meliksah Yorulmazlar <yorulk@rpi.edu>
2025-01-12 01:24:00 -0500

    Update main.py

--- Newspapers/Bulgaria/Duma/main.py
+++ Newspapers/Bulgaria/Duma/main.py
@@ -1,164 +1,141 @@
-#Duma webscraper.Duma is a newspaper in Bulgaria (Duma in Bulgarian means word)
-
-
-import requests,os,lxml,datetime
+#Kultura Webscraper.Kultura is a Bulgarian magazine(In Bulgarian, Kultura means culture)
+import requests,os,lxml
 from bs4 import BeautifulSoup
 
-
-class Duma:
+class Kultura:
     def __init__(self):
-        #initial start date of the archive: 5th January 2009
-        self.initial_date = datetime.datetime(day=5,month=1,year=2009)
-        self.one_day = datetime.timedelta(days=1)
-        self.get_today()
+        self.links = []
+        self.years = []
+        self.get_years()
 
 
-    #finds the date of today
-    #Do not use the method,the method is used for initialisation of the class
-    def get_today(self):
-        d = datetime.datetime.now().day
-        m = datetime.datetime.now().month
-        y = datetime.datetime.now().year
-        self.final_date  = datetime.datetime(day=d,month=m,year=y)
+    #This will find all the years on the website
+    def get_years(self):
+        website = f"https://newspaper.kultura.bg/bg/archive/view/"
+        soup = BeautifulSoup(requests.get(url=website).text,"lxml")
+        select_tag = soup.find("select",class_="one_option",id="archive_year")
+        self.years= [int(option.text) for option in select_tag.find_all("option")]
+        self.years = self.years[::-1]
 
-    #This will download all of the Duma newspapers from the 5th February 2009 till today
-    def download_all(self):
-        start = self.initial_date
-        end = self.final_date
-        while start <= end:
-            self.download_date(start)
-            start += self.one_day
+    #This method will show all the years that you can choose from to download for the download_year method
+    def show_years(self):
+        print(self.years)
 
-    #This will download any year between 2009 and the year that we are currently in
+    #This method downloads all the magazines for that year is that year is on the website
+    #the input year should be an integer
+    #example usage: k.download_year(year="1999")
     def download_year(self,year:int):
-        if year >= 2009:
-            initial_date = datetime.datetime(day=1,month=1,year=year)
-            final_date = datetime.datetime(day=31,month=12,year=year)
-            while initial_date <= final_date:
-                self.download_date(initial_date)
-                initial_date += self.one_day
+        if year in self.years:
+            website = f"https://newspaper.kultura.bg/bg/archive/view/{year}"
+            soup = BeautifulSoup(requests.get(url=website).text,"lxml")
+            links = [link['href'] for link in soup.find_all("a",href=True) if "htm" not in link["href"] and "https://newspaper.kultura.bg/bg/home/view/" in link["href"]]
+            os.makedirs(str(year))
+            print(links)
+            for link in links:
+                link_soup = BeautifulSoup(requests.get(url=link).text,"lxml")
+                pdf_link = link_soup.find("a",target=True,class_="pdf")
 
-    #tuple(day,month,year)
-    #lets say you want to download from 9th March 2018 til 15th september 2019
-    #download(date1=(9,3,2018),date2=(15,9,2019)) is an example usage of the following method
-    def download_d1_d2(self,date1:datetime.datetime,date2:datetime.datetime):
-        if date1 > date2:
-            c = date1
-            date1 = date2
-            date2 = c
+                #This if statement will check if there is a pdf or not
+                if pdf_link is not None:
+                    pdf_link = pdf_link["href"]
+                    #https://newspaper.kultura.bg/media/file/{}<-- name of the pdf within those curly braces
 
-        initial_date = date1
-        final_date = date2
-        year = date1[2]
-        if year >= 2009:
-            if initial_date < final_date:
-                while initial_date <= final_date:
-                    self.download_date(initial_date)
-                    initial_date += self.one_day
+                    response = requests.get(url=pdf_link)
+                    filename = pdf_link.split("/")[-1]
 
-    #This will download the newspaper for today
-    def download_today(self):
-        self.download_date(date=self.final_date)
+                    if response.status_code == 200:
+                        with open(f"{year}/{filename}","wb") as f:
+                            f.write(response.content)
+                        with open("download_results.txt","a") as f:
+                            f.write(f"{filename} was downloaded\n")
+                            print(f"{filename} was downloaded")
+                    else:
+                        with open("download_results.txt","a") as f:
+                            f.write(f"{filename} was not downloaded,it had response status code {response.status_code}\n")
+                            print(f"{filename} was downloaded,it had response status code {response.status_code}")
 
-    #The following method will download a specific date
-    def download_date(self,date:datetime.datetime):
-        day = date.day
-        month = date.month
-        year = date.year
-        website = f"https://duma.bg/?go=newspaper&p=list&year={year}&month={month}&day={day}"
+    #This method will download all the years on the website
+    #It takes no input
+    def download_years(self):
+        first_year = self.years[0]
+        last_year = self.years[1]
+        self.download_year1_year2(year1=first_year,year2=last_year)
 
-        soup = BeautifulSoup(requests.get(url=website).text, "lxml")
-        try:
-            pdf_link = soup.find("a", href=True, class_="dwnl_newspaper_item")["href"]
-        except TypeError:
-            pass
-        else:
-            pdf_link = "https://duma.bg/" + pdf_link
-            filename = f"{day}-{month}-{year}.pdf"
+    #This will download all the pdfs from one specified year to another
+    def download_year1_year2(self,year1:int,year2:int):
+        if year1 > year2:
+            c = year1
+            year1 = year2
+            year2 = c
+        for year in range(year1,year2+1):
+            self.download_year(year)
 
-            response = requests.get(url=pdf_link)
-            if response.status_code == 200:
-                with open(f"{filename}", "wb") as f:
-                    f.write(response.content)
-                with open("download_results.txt", 'a') as f:
-                    f.write(f"{filename} was downloaded\n")
-                print(f"{filename} was downloaded")
-            else:
-                with open("download_results.txt", 'a') as f:
-                    f.write(f"{filename} was not downloaded, it had status code {response.status_code}\n")
-                print(f"{filename}  was not downloaded, it had status code {response.status_code}\n")
+    # The following method will check if the pdfs for a particular year have been downloaded or not
+    def check_year(self,year:int):
+        if year in self.years:
+            website = f"https://newspaper.kultura.bg/bg/archive/view/{year}"
+            soup = BeautifulSoup(requests.get(url=website).text,"lxml")
+            links = [link['href'] for link in soup.find_all("a",href=True) if "htm" not in link["href"] and "https://newspaper.kultura.bg/bg/home/view/" in link["href"]]
+            os.makedirs(str(year))
+            print(links)
+            for link in links:
+                link_soup = BeautifulSoup(requests.get(url=link).text,"lxml")
+                pdf_link = link_soup.find("a",target=True,class_="pdf")
 
-    # This method will check if the pdf for a particular date has been downloaded or nor
-    def check_date(self,date:datetime.datetime):
-        day = date.day
-        month = date.month
-        year = date.year
-        website = f"https://duma.bg/?go=newspaper&p=list&year={year}&month={month}&day={day}"
+                #This if statement will check if there is a pdf or not
+                if pdf_link is not None:
+                    pdf_link = pdf_link["href"]
+                    #https://newspaper.kultura.bg/media/file/{}<-- name of the pdf within those curly braces
 
-        soup = BeautifulSoup(requests.get(url=website).text, "lxml")
-        try:
-            pdf_link = soup.find("a", href=True, class_="dwnl_newspaper_item")["href"]
-        except TypeError:
-            pass
-        else:
-            pdf_link = "https://duma.bg/" + pdf_link
-            filename = f"{day}-{month}-{year}.pdf"
-            if filename not in os.listdir():
-                response = requests.get(url=pdf_link)
-                if response.status_code == 200:
-                    with open(f"{filename}", "wb") as f:
-                        f.write(response.content)
-                    with open("download_results.txt", 'a') as f:
-                        f.write(f"{filename} was downloaded\n")
-                    print(f"{filename} was downloaded")
-                else:
-                    with open("download_results.txt", 'a') as f:
-                        f.write(f"{filename} was not downloaded, it had status code {response.status_code}\n")
-                    print(f"{filename}  was not downloaded, it had status code {response.status_code}\n")
 
-    #The following method will check from one date to another
-    def check_d1_d2(self,date1:datetime.datetime,date2:datetime.datetime):
-        if date1 > date2:
-            c = date1
-            date1 = date2
-            date2 = c
+                    filename = pdf_link.split("/")[-1]
+                    if filename not in os.listdir(f"{year}"):
+                        response = requests.get(url=pdf_link)
+                        if response.status_code == 200:
+                            with open(f"{year}/{filename}","wb") as f:
+                                f.write(response.content)
+                            with open("download_results.txt","a") as f:
+                                f.write(f"{filename} was downloaded\n")
+                                print(f"{filename} was downloaded")
+                        else:
+                            with open("download_results.txt","a") as f:
+                                f.write(f"{filename} was not downloaded,it had response status code {response.status_code}\n")
+                                print(f"{filename} was downloaded,it had response status code {response.status_code}")
 
-        initial_date = date1
-        final_date = date2
-        year = date1[2]
-        if year >= 2009:
-            if initial_date < final_date:
-                while initial_date <= final_date:
-                    self.check_date(initial_date)
-                    initial_date += self.one_day
+    #The following method will check from one particular year to another particular year
+    def check_year1_year2(self,year1:int,year2:int):
+        if year1 > year2:
+            c = year1
+            year1 = year2
+            year2 = c
+        for year in range(year1,year2+1):
+            self.check_year(year)
 
-    #The following method will check all the dates to see if they have been downloaded or not
-    def check_all(self):
-        date1 = self.initial_date
-        date2 = self.final_date
-        self.check_d1_d2(date1,date2)
+    #This method will check if all the pdfs have been downloaded or not
+    def check_years(self):
+        first_year = min(self.years)
+        last_year = max(self.years)
+        self.check_year1_year2(first_year,last_year)
 
 
 if __name__ == "__main__":
-    d = Duma()
+    k = Kultura()
 
-    #The following method will download today's paper
+    #This method will download all the years on the website
     #Takes no input
-    #d.download_today()
+    #k.download_years()
 
-    #The following method will download all the newspapers for that year(any year from 2009 to the current year)
-    #The input should be an integer
-    #d.download_year(year=2010)
+    #This method will show all the years that you can choose from to download
+    #Takes no input
+    #k.show_years()
 
-    #This will download all the newspapers from January 5 2009 till today
-    #takes no input
-    #d.download_all()
+    #This method will download all the pdfs in a year found on the website
+    #the year has to be an integer
+    #k.download_year(year=1999)
 
-    #The following method will download all the newspapers from date1 to date2
-    #date2 should be after date1
-    #the inputs for date1 and date2 should be as a tuple
-    #if date1 is 1st october 2018 and date2 is 30th June 2021
-    #date1 should be (1,10,2018) and date2 should be (30,6,2021)
-    #d.download(date1=(1,10,2018),date2=(30,6,2021))
-
+    #This method will download all the pdfs from one year to another
+    #the two arguments have to be an integer
+    #k.download_year1_year2(year1=1990,year2=2000) 
+    #will download the following years:
+    #1990 1991 1992 1993 1994 1995 1996 1997 1998 1999 2000
 
