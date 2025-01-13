695735f3ed6552176e9def82600a27c65ca61c60
Meliksah Yorulmazlar <yorulk@rpi.edu>
2025-01-13 05:16:50 -0500

    Update main.py

--- Newspapers/Portugal/A Comarca de Arganil/main.py
+++ Newspapers/Portugal/A Comarca de Arganil/main.py
@@ -115,19 +115,66 @@ class AComarcadeArganil:
     def download_all(self):
         self.download_n1_n2(self.minimum,self.maximum)
 
+    #The following method will check if a number was downloaded or not
+    def check_number(self,number:int):
+        if self.minimum <= number <= self.maximum:
+            try:
+                os.mkdir(self.name)
+            except FileExistsError:
+                pass
+            dictionary = {11881:"11881-20090422.pdf",11882:"20090429",11883:"20090506",11884:"20090513",11885:"20090520",11886:"20090527",11887:"20090603",11888:"0090610"}
+            if number in dictionary:
+                filename = f"{number}-{dictionary[number]}.pdf"
+            else:
+                page_number = self.find_current_page(number)
+                website = f"https://www.acomarcadearganil.cm-arganil.pt/edicoes-da-comarca/page/{page_number}/"
+                soup = BeautifulSoup(requests.get(url=website).text,"lxml")
+                divs = [div.text.replace("\n","") for div in soup.find_all("div") if "Data de Edição: " in div.text]
+                print(divs[::-1])
+                dates = []
+                index = len(divs)-1
+                while len(dates) <60:
+                    date = divs[index].strip("Data de Edição: ").replace("/","")
+                    dates.append(date)
+                    index -= 1
+                dates = dates[::-1]
+                remainder = number%60
+                if remainder == 0:
+                    filename = f'{number}-{dates[59]}.pdf'
+                else:
+                    filename = f'{number}-{dates[remainder - 1]}.pdf'
+
+            pdf_page = f"https://www.acomarcadearganil.cm-arganil.pt/comarcafiles/{number}/{number}.pdf"
+
+            if filename not in os.listdir(self.name):
+                response = requests.get(url=pdf_page)
+
+                if response.status_code == 200:
+                    with open(f"{self.name}/{filename}","wb") as f:
+                        f.write(response.content)
+                    with open("download_results.txt","a") as f:
+                        f.write(f"{number} was downloaded\n")
+                    print(f"{number} was downloaded")
+                else:
+                    with open("download_results.txt","a") as f:
+                        f.write(f"{number} was not downloaded,it had response status code {response.status_code}\n")
+                    print(f"{number} was not downloaded,it had response status code {response.status_code}")
+
+    #The following method will check from one number to another larger number
+    def check_n1_n2(self,n1:int,n2:int):
+        if n1 > n2:
+            c = n1
+            n1 = n2
+            n2 = c
+        for i in range(n1,n2+1):
+            self.check_number(i)
+
+    # The following method will check the entire archive
+    def check_all(self):
+        self.check_n1_n2(self.minimum,self.maximum)
+
+
 
 if __name__ == "__main__":
     aca = AComarcadeArganil()
     aca.download_n1_n2(11883,aca.maximum)
-
-
-
-
-
-
-
-
-
-
-
-
