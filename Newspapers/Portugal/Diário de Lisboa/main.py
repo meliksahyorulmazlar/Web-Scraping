00e4c33a288e3e97a3aa3cfb35e01c6e9aa7632e
Meliksah Yorulmazlar <yorulk@rpi.edu>
2025-01-13 05:17:19 -0500

    Update main.py

--- Newspapers/Portugal/Diário de Lisboa/main.py
+++ Newspapers/Portugal/Diário de Lisboa/main.py
@@ -26,7 +26,12 @@ class DiariodeLisboa:
                 index = days.index(date.day)
                 link = links[index]
                 print(link)
-                self.get_images(date,link)
+                soup = BeautifulSoup(requests.get(url=link).text, "lxml")
+                images = [image["src"] for image in soup.find_all("img", alt=True)]
+                images = images[4:]
+                images = [img.replace("d3", "d2").replace("D3", "D2").replace("jpg", "png") for img in images]
+                for i in range(len(images)):
+                    self.download_image(date, images[i], i + 1)
             else:
                 with open("download_results.txt", "a") as f:
                     f.write(f"{date.day}-{date.month}-{date.year} is not on the archive\n")
@@ -36,14 +41,7 @@ class DiariodeLisboa:
                 f.write(f"{date.day}-{date.month}-{date.year} is a date either before the archive began or after it finished\n")
             print(f"{date.day}-{date.month}-{date.year} is a date either before the archive began or after it finished")
 
-    # The following method when given a link will find all the images
-    def get_images(self,date:datetime.datetime,link:str):
-        soup = BeautifulSoup(requests.get(url=link).text,"lxml")
-        images = [image["src"] for image in soup.find_all("img",alt=True)]
-        images = images[4:]
-        images = [img.replace("d3","d2").replace("D3","D2").replace("jpg","png") for img in images]
-        for i in range(len(images)):
-            self.download_image(date,images[i],i+1)
+
 
     # The following method will download an image when given a link
     def download_image(self,date:datetime.datetime,image_link:str,count:int):
@@ -92,6 +90,83 @@ class DiariodeLisboa:
     def download_all(self):
         self.download_d1_d2(d1=self.start_date,d2=self.end_date)
 
+    # The following method will check if a date was downloaded or not.If it was not downloaded, this method will download it
+    def check_date(self,date:datetime.datetime):
+        if self.start_date <= date <= self.end_date:
+            try:
+                os.mkdir(str(date.year))
+            except FileExistsError:
+                pass
+            website = f"http://casacomum.org/cc/diario_de_lisboa/dia?ano={date.year}&mes={date.month}"
+            soup = BeautifulSoup(requests.get(url=website).text,"lxml")
+            days = [int(span.text) for span in soup.find_all("span",class_="dia_do_mes_sobre")]
+
+            if date.day in days:
+                links = [self.replace_href(link["href"]) for link in soup.find_all("a",href=True) if 'pasta' in link['href']]
+                index = days.index(date.day)
+                link = links[index]
+                print(link)
+                soup = BeautifulSoup(requests.get(url=link).text, "lxml")
+                images = [image["src"] for image in soup.find_all("img", alt=True)]
+                images = images[4:]
+                images = [img.replace("d3", "d2").replace("D3", "D2").replace("jpg", "png") for img in images]
+                for i in range(len(images)):
+                    self.check_image(date, images[i], i + 1)
+            else:
+                with open("download_results.txt", "a") as f:
+                    f.write(f"{date.day}-{date.month}-{date.year} is not on the archive\n")
+                print(f"{date.day}-{date.month}-{date.year} is not on the archive")
+        else:
+            with open("download_results.txt", "a") as f:
+                f.write(f"{date.day}-{date.month}-{date.year} is a date either before the archive began or after it finished\n")
+            print(f"{date.day}-{date.month}-{date.year} is a date either before the archive began or after it finished")
+
+    # The following method will check the images to see if they were downloaded or not
+    def check_image(self,date:datetime.datetime,image_link:str,count:int):
+        formatted_date = f"{date.day}-{date.month}-{date.year}"
+        try:
+            os.mkdir(f"{date.year}/{formatted_date}")
+        except FileExistsError:
+            pass
+
+        if f"{count}.png" not in os.listdir(f"{date.year}/{formatted_date}"):
+            response = requests.get(url=image_link)
+            if response.status_code == 200:
+                with open(f"{date.year}/{formatted_date}/{count}.png", "wb") as f:
+                    f.write(response.content)
+                with open("download_results.txt", "a") as f:
+                    f.write(f"{date.year}/{formatted_date}/{count}.png was downloaded\n")
+                print(f"{date.year}/{formatted_date}/{count}.png was downloaded")
+            else:
+                image_link = image_link.replace(".png", ".jpg")
+                new_response = requests.get(url=image_link)
+                if new_response.status_code == 200:
+                    with open(f"{date.year}/{formatted_date}/{count}.png", "wb") as f:
+                        f.write(new_response.content)
+                    with open("download_results.txt", "a") as f:
+                        f.write(f"{date.year}/{formatted_date}/{count}.png was downloaded\n")
+                    print(f"{date.year}/{formatted_date}/{count}.png was downloaded")
+                else:
+                    with open("download_results.txt", "a") as f:
+                        f.write(
+                            f"{date.year}/{formatted_date}/{count}.png was not downloaded,it had response status code {new_response.status_code}\n")
+                    print(
+                        f"{date.year}/{formatted_date}/{count}.png was not downloaded,it had response status code {new_response.status_code}")
+
+    # The following method will check from one date to another later date
+    def check_d1_d2(self,d1:datetime.datetime,d2:datetime.datetime):
+        if d1 > d2:
+            c = d1
+            d1 = d2
+            d2 = c
+
+        while d1 <= d2:
+            self.check_date(d1)
+            d1 += self.one_day
+
+    # The following method will check the entire archive
+    def check_all(self):
+        self.check_d1_d2(self.start_date,self.end_date)
 
 if __name__ == "__main__":
     dl = DiariodeLisboa()
@@ -105,8 +180,3 @@ if __name__ == "__main__":
     # The following method will download all the images from one date to another
     # It will end up downloading all the images from the 4th May 1921 till the 18th May 1921
     dl.download_d1_d2(d1=datetime.datetime(day=4,month=5,year=1921),d2=datetime.datetime(day=18,month=5,year=1921))
-
-
-
-
-
