d40a600abe791ffcc30164de2a279e14644ace5c
Meliksah Yorulmazlar <yorulk@rpi.edu>
2025-01-13 02:41:52 -0500

    Update main.py

--- Newspapers/Turkey/Erzurum Gazetesi/main.py
+++ Newspapers/Turkey/Erzurum Gazetesi/main.py
@@ -67,6 +67,54 @@ class ErzurumGazetesi:
     def download_all(self):
         self.download_d1_d2(self.first,self.today)
 
+    # The following method will check the download of a particular date
+    def check_date(self,date:datetime.datetime):
+        if self.first <= date <= self.today:
+            day = date.day
+            month = date.month
+            year = date.year
+            page = f"http://www.erzurumgazetesi.com.tr/default.asp?page=arsiv&gun={day}&ay={month}&yil={year}&ge=Getir"
+            soup = BeautifulSoup(requests.get(url=page).text,"lxml")
+            links = [f"{self.main}{link['href']}" for link in soup.find_all("a",href=True,class_="fancybo")]
+            formatted_date = f"{day}-{month}-{year}"
+            if len(links) == 0:
+                with open("download_results.txt","a") as f:
+                    f.write(f"{formatted_date} had no links on the archive\n")
+                print(f"{formatted_date} had no links on the archive")
+            else:
+                try:
+                    os.mkdir(formatted_date)
+                except FileExistsError:
+                    pass
+                for link in links:
+                    print(link)
+                    filename = link.split("/")[-1]
+                    if filename not in os.listdir(formatted_date):
+                        response = requests.get(url=link)
+                        if response.status_code == 200:
+                            with open(f"{formatted_date}/{filename}","wb") as f:
+                                f.write(response.content)
+                            with open(f"download_results.txt","a") as f:
+                                f.write(f"{formatted_date}/{filename} was downloaded\n")
+                            print(f"{formatted_date}/{filename} was downloaded")
+                        else:
+                            with open(f"download_results.txt","a") as f:
+                                f.write(f"{formatted_date}/{filename} was not downloaded,it had response status code {response.status_code}\n")
+                            print(f"{formatted_date}/{filename} was not downloaded,it had response status code {response.status_code}")
+
+    #This method will download all the newspapers from one date till another date
+    def check_d1_d2(self,d1:datetime.datetime,d2:datetime.datetime):
+        if d1 > d2:
+            c = d2
+            d2 = d1
+            d1 = c
+        while d1 <= d2:
+            self.download_date(d1)
+            d1 += self.one_day
+
+    #This method will download all the archive
+    def check_all(self):
+        self.check_d1_d2(self.first,self.today)
 
 if __name__ == "__main__":
     eg = ErzurumGazetesi()
