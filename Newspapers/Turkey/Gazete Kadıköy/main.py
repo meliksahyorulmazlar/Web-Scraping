4ab498fe483280efa2e3837b5405f88996df0498
Meliksah Yorulmazlar <yorulk@rpi.edu>
2025-01-13 02:45:38 -0500

    Update main.py

--- Newspapers/Turkey/Gazete Kadıköy/main.py
+++ Newspapers/Turkey/Gazete Kadıköy/main.py
@@ -10,6 +10,7 @@ class GazeteKadikoy:
     def __init__(self):
         self.first = 1
         self.last = self.find_last()
+        print(f"{self.last} is the last number")
         self.links = self.get_all_links()[::-1]
 
     # This will find the number of the last page of the archive
@@ -20,8 +21,7 @@ class GazeteKadikoy:
         while not found:
             page = f"https://www.gazetekadikoy.com.tr/e-gazete?page={count}&filter=&filter="
             soup = BeautifulSoup(requests.get(url=page).text, "lxml")
-            pages = [int(link.text) for link in soup.find_all("a", href=True, class_="page-link") if
-                     '«' not in link.text and '»' not in link.text]
+            pages = [int(link.text) for link in soup.find_all("a", href=True, class_="page-link") if '«' not in link.text and '»' not in link.text]
             print(pages)
             largest = max(pages)
             if count > largest:
@@ -128,12 +128,57 @@ class GazeteKadikoy:
     def download_number(self, number: int):
         for link in self.links:
             if link[0] == number:
-                self.download_n1_n2(number, number + 1)
+                self.download_tuple(link)
 
     # This method will download the latest copy of Gazete Kadıköy
     def download_latest(self):
         self.download_tuple(self.links[-1])
 
+    # The following method will check a specific number
+    def check_number(self,number:int):
+        for link in self.links:
+            if link[0] == number:
+                self.check_tuple(link)
+
+    # The following method will check if the file had been downloaded before or not
+    def check_tuple(self,link:tuple):
+        count = str(count_link[0])
+        link = count_link[1]
+        print(count)
+        try:
+            os.mkdir(count)
+        except FileExistsError:
+            pass
+
+        if f"{count}.pdf" not in os.listdir(count):
+            response = requests.get(url=link)
+
+            if response.status_code == 200:
+                with open(f"{count}/{count}.pdf", "wb") as f:
+                    f.write(response.content)
+                with open("download_results.txt", "a") as f:
+                    f.write(f"{count}.pdf was downloaded\n")
+                print(f"{count}.pdf was downloaded")
+            else:
+                with open("download_results.txt", "a") as f:
+                    f.write(f"{count}.pdf was not downloaded,it had response status code {response.status_code}\n")
+                print(f"{count}.pdf was not downloaded,it had response status code {response.status_code}")
+
+    # The following method will check from one number to another larger number
+    def check_n1_n2(self,n1:int,n2:int):
+        if n1 > n2:
+            c = n1
+            n1 = n2
+            n2 = c
+        for i in range(n1,n2):
+            self.check_number(i)
+
+    # The following method will check the entire archive
+    def check_all(self):
+        numbers = [n[0] for n in self.links]
+        n1 = min(numbers)
+        n2 = max(numbers)
+        self.check_n1_n2(n1,n2)
 
 if __name__ == "__main__":
     gk = GazeteKadikoy()
