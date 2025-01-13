c54a285ee80f73f78b70888b2eb65bd016c1d1b9
Meliksah Yorulmazlar <yorulk@rpi.edu>
2025-01-13 03:18:37 -0500

    Update main.py

--- Newspapers/Iran/Noor-e Khuzestan/main.py
+++ Newspapers/Iran/Noor-e Khuzestan/main.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 
 class NooreKhuzestan:
     def __init__(self):
+        self.first = 5706
         self.latest = self.find_latest()
 
 
@@ -21,49 +22,46 @@ class NooreKhuzestan:
                 return number
 
     #This method will download ith numbered newspapers pages
-    def download(self,i):
-        number = str(i)
-        page = f"http://noordaily.ir/?nid={number}&pid=1&type=0"
-        soup = BeautifulSoup(requests.get(url=page).text, "lxml")
-        links = list(set([anchor["href"] for anchor in soup.find_all("a", href=True) if "pid" in anchor["href"]]))
-        links.sort()
-        links = [f"http://noordaily.ir{link}" for link in links]
-        print(links)
-        try:
-            os.makedirs(number)
-        except FileExistsError:
-            pass
+    def download(self,i:int):
+        if self.first <= i <= self.latest:
+            number = str(i)
+            page = f"http://noordaily.ir/?nid={number}&pid=1&type=0"
+            soup = BeautifulSoup(requests.get(url=page).text, "lxml")
+            links = list(set([anchor["href"] for anchor in soup.find_all("a", href=True) if "pid" in anchor["href"]]))
+            links.sort()
+            links = [f"http://noordaily.ir{link}" for link in links]
+            print(links)
+            try:
+                os.mkdir(number)
+            except FileExistsError:
+                pass
 
-        for i in range(len(links)):
-            link = links[i]
-            page_number = i + 1
-            filename = f"{number}_{page_number}.jpg"
-            content = requests.get(url=link)
-            new_soup = BeautifulSoup(content.text, 'lxml')
-            images = new_soup.find_all("img", src=True)
-            image = [f'http://noordaily.ir{image["src"]}' for image in new_soup.find_all("img", src=True) if "/content/newspaper/Version" in image["src"]][0]
-            print(image)
+            for i in range(len(links)):
+                link = links[i]
+                page_number = i + 1
+                filename = f"{number}_{page_number}.jpg"
+                content = requests.get(url=link)
+                new_soup = BeautifulSoup(content.text, 'lxml')
+                images = new_soup.find_all("img", src=True)
+                image = [f'http://noordaily.ir{image["src"]}' for image in new_soup.find_all("img", src=True) if "/content/newspaper/Version" in image["src"]][0]
+                print(image)
 
-            response = requests.get(url=image)
-            if response.status_code == 200:
-                with open(f"{number}/{filename}", "wb") as f:
-                    f.write(response.content)
-                with open("download_results.txt", "a") as f:
-                    f.write(f"{number}/{filename} was downloaded\n")
-                print(f"{number}/{filename} was downloaded")
-            else:
-                with open("download_results.txt", "a") as f:
-                    f.write(f"{number}/{filename} was not downloaded {response.status_code}\n")
-                print(f"{number}/{filename} was not downloaded {response.status_code}")
+                response = requests.get(url=image)
+                if response.status_code == 200:
+                    with open(f"{number}/{filename}", "wb") as f:
+                        f.write(response.content)
+                    with open("download_results.txt", "a") as f:
+                        f.write(f"{number}/{filename} was downloaded\n")
+                    print(f"{number}/{filename} was downloaded")
+                else:
+                    with open("download_results.txt", "a") as f:
+                        f.write(f"{number}/{filename} was not downloaded {response.status_code}\n")
+                    print(f"{number}/{filename} was not downloaded {response.status_code}")
 
     #The following method will download all the pdfs
     def download_all(self):
-        for i in range(5706,self.latest+1):
+        for i in range(self.first,self.latest+1):
             self.download(i)
-
-    #This method will download a specifically specified number pdf
-    def download_number(self,number:int):
-        self.download(number)
 
     #This method will download all the newspapers from n1 till n2
     #For example
@@ -77,6 +75,59 @@ class NooreKhuzestan:
             n1 = c
         for i in range(n1,n2+1):
             self.download(i)
+
+    # The following method will check if a specific number has been downloaded or not
+    def check(self,i:int):
+        if self.first <= i <= self.latest:
+            number = str(i)
+            page = f"http://noordaily.ir/?nid={number}&pid=1&type=0"
+            soup = BeautifulSoup(requests.get(url=page).text, "lxml")
+            links = list(set([anchor["href"] for anchor in soup.find_all("a", href=True) if "pid" in anchor["href"]]))
+            links.sort()
+            links = [f"http://noordaily.ir{link}" for link in links]
+            print(links)
+            try:
+                os.mkdir(number)
+            except FileExistsError:
+                pass
+
+            for i in range(len(links)):
+                link = links[i]
+                page_number = i + 1
+                filename = f"{number}_{page_number}.jpg"
+                if filename not in os.listdir(number):
+                    content = requests.get(url=link)
+                    new_soup = BeautifulSoup(content.text, 'lxml')
+                    images = new_soup.find_all("img", src=True)
+                    image = [f'http://noordaily.ir{image["src"]}' for image in new_soup.find_all("img", src=True) if "/content/newspaper/Version" in image["src"]][0]
+                    print(image)
+
+                    response = requests.get(url=image)
+                    if response.status_code == 200:
+                        with open(f"{number}/{filename}", "wb") as f:
+                            f.write(response.content)
+                        with open("download_results.txt", "a") as f:
+                            f.write(f"{number}/{filename} was downloaded\n")
+                        print(f"{number}/{filename} was downloaded")
+                    else:
+                        with open("download_results.txt", "a") as f:
+                            f.write(f"{number}/{filename} was not downloaded {response.status_code}\n")
+                        print(f"{number}/{filename} was not downloaded {response.status_code}")
+
+    # The following method will check from the n1th till the n2nd copy to see if the number has been downloaded or not
+    def check_n1_n2(self,n1,n2):
+        if n1>n2:
+            c = n2
+            n2 = n1
+            n1 = c
+        for i in range(n1,n2+1):
+            self.download(i)
+
+    # The following method will check the entire archive
+    def check_all(self):
+        start = self.first
+        end = self.latest
+        self.check_n1_n2(start,end)
 
 if __name__ == "__main__":
     nk = NooreKhuzestan()
