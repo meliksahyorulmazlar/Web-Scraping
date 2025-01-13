c7c520f43ef8653a3d53abd42bdec92fe311c75b
Meliksah Yorulmazlar <yorulk@rpi.edu>
2025-01-13 02:46:34 -0500

    Update main.py

--- Newspapers/Turkey/Yeni Asır/main.py
+++ Newspapers/Turkey/Yeni Asır/main.py
@@ -6,7 +6,6 @@ import requests,os,lxml,datetime
 from bs4 import BeautifulSoup
 
 
-
 class YeniAsir:
     def __init__(self):
         self.start_date = datetime.datetime(day=1,month=1,year=2021)
@@ -68,6 +67,50 @@ class YeniAsir:
     #This method will download today's front page of Takvim
     def download_today(self):
         self.download_date(date=self.today)
+
+    # The following method will check a particular date to check if it has been downloaded or nor
+    def check_date(self,date:datetime.datetime):
+        if date < self.start_date or date > self.today:
+            print("The date is not on the archive or the date is in the future.")
+        else:
+
+            day = date.day
+            month = date.month
+            year = date.year
+            website = f"https://egazete.yeniasir.com.tr/eGazete/image/www_yeniasir_com_tr/{year}/{month}/{day}/1"
+            formatted_date = f"{day}-{month}-{year}"
+            if f"{formatted_date}.jpeg" not in os.listdir():
+                response = requests.get(url=website)
+                if len(response.content) >100:
+                    if response.status_code == 200:
+                        with open(f"{formatted_date}.jpeg", "wb") as f:
+                            f.write(response.content)
+                        with open("download_results.txt", "a") as f:
+                            f.write(f"{formatted_date} was downloaded\n")
+                        print(f"{formatted_date} was downloaded")
+                    else:
+                        with open("download_results.txt", "a") as f:
+                            f.write(f"{formatted_date} was not downloaded,it had response status code {response.status_code}\n")
+                        print(f"{formatted_date} was not downloaded,it had response status code {response.status_code}")
+                else:
+                    with open("download_results.txt", "a") as f:
+                        f.write(f"{formatted_date} was not on the archive, it was only {len(response.content)} bytes\n")
+                    print(f"{formatted_date} was not on the archive, it was only {len(response.content)} bytes")
+
+    # The following method will check from one date to another later date
+    def check_d1_d2(self,d1:datetime.datetime,d2:datetime.datetime):
+        if d1 > d2:
+            c = d1
+            d1 = d2
+            d2 = c
+
+        while d1 <= d2:
+            self.check_date(d1)
+            d1 += self.one_day
+
+    # The following method will check the entire archive
+    def check_all(self):
+        self.check_d1_d2(self.start_date,self.today)
 
 
 
