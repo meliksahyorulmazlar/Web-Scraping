b7f56a18e9e4c1d3459bc47db57dfe9412d5aeaf
Meliksah Yorulmazlar <yorulk@rpi.edu>
2025-02-17 23:51:45 -0500

    Update main.py
    does not enter an infinite loop anymore

--- Newspapers/Brunei/Pelita Brunei/main.py
+++ Newspapers/Brunei/Pelita Brunei/main.py
@@ -8,9 +8,6 @@ import requests,os,lxml
 from bs4 import BeautifulSoup
 
 
-
-
-
 class PelitaBrunei:
     def __init__(self):
         self.first_year = 1992
@@ -118,6 +115,7 @@ class PelitaBrunei:
             pages = []
             pdfs = []
             count = 0
+            extra_count = 0
             website = f'https://www.pelitabrunei.gov.bn/Arkib%2520Dokumen/Forms/AllItems.aspx?RootFolder=%252fArkib%2520Dokumen%252f{year}%23PageFirstRow=50#InplviewHash9ac62886-5881-4af7-baf9-999d90c15f2a=Paged%3DTRUE-p_SortBehavior%3D0-p_Created%3D20201004%252023%253a50%253a23-p_ID%3D3046-PageFirstRow%3D{count + 1}'
             print(website)
             self.driver.get(website)
@@ -133,6 +131,8 @@ class PelitaBrunei:
                             pdfs.append(pdf['href'])
                         new_count += 1
                 if new_count == 0:
+                    if extra_count > 0:
+                        return []
                     directories = [f"https://www.pelitabrunei.gov.bn" + l['href'] for l in soup.find_all('a', class_='ms-listlink')]
                     for directory in directories:
                         self.driver.get(directory)
@@ -143,6 +143,7 @@ class PelitaBrunei:
                                 if pdf['href'] not in pdfs:
                                     pdfs.append(pdf['href'])
                                 new_count += 1
+                    extra_count += 1
                 else:
                     tags = [tag.get_attribute('onclick') for tag in self.driver.find_elements(By.TAG_NAME, 'a') if
                             tag.get_attribute('onclick')]
